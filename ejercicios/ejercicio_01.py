ba63fa17eb13ca1097a1a9da501e6b9c3ec127bf
Mayra Alzate <nadja.minni@gmail.com>
2026-02-28 16:21:13 -0500

    Ejercicio 1

--- ejercicios/ejercicio_01.py
+++ ejercicios/ejercicio_01.py
@@ -3,11 +3,28 @@
 # Ejercicio 1: Escribe un programa que imprima en pantalla el mensaje "Hola, Mundo!".
 # Escribe tu código debajo de esta línea:
 
+print()
+
+frase = "Hola, Mundo!"
+print(frase)
+print()
+
 
 # Ejercicio 2: Usa la función print() para mostrar el nombre de tu película o libro favorito.
 # Escribe tu código debajo de esta línea:
 
+pelicula_fav = "Joy"
+print (f"Mi película favorita es {pelicula_fav}")
+print()
 
 # Ejercicio 3: Imprime tres palabras diferentes en una sola línea utilizando un solo print().
 # Escribe tu código debajo de esta línea:
+
+nombre = "Ana"
+edad = 5
+ciudad = "Barranquilla"
+
+print(f"Ella se llama {nombre}, tiene {edad} años y vive en {ciudad}.")
+
+
 
