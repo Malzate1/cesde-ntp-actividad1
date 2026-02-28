888e7789a19a23693070cee6fc821ed197bcc0ec
Mayra Alzate <nadja.minni@gmail.com>
2026-02-28 17:16:45 -0500

    Ejercicio 5

--- ejercicios/ejercicio_05.py
+++ ejercicios/ejercicio_05.py
@@ -4,12 +4,33 @@
 # los números del 1 al 10.
 # Escribe tu código debajo de esta línea:
 
+for i in range(1,11):
+    print(i)
+
+print()
+
 
 # Ejercicio 2: Utiliza un ciclo for para imprimir todos los números pares del 2 al 10 inclusive.
 # Escribe tu código debajo de esta línea:
+
+for i in range(2,11,2):
+    print(i)
+print()
+
+
 
 
 # Ejercicio 3: Usa un ciclo while para calcular la suma de los números del 1 al 5. Imprime el 
 # resultado final (debe ser 15).
 # Escribe tu código debajo de esta línea:
+suma =0
+num=1
+
+while num <=5:
+    suma = suma + num
+    num = num + 1
+
+print(f"La suma es igual a {suma}.")
+
+
 
