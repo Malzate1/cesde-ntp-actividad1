f32482c7f49aadd792443fd2e16b69425bb26cf4
Mayra Alzate <nadja.minni@gmail.com>
2026-02-28 17:45:33 -0500

    Ejercicio 6

--- ejercicios/ejercicio_06.py
+++ ejercicios/ejercicio_06.py
@@ -2,17 +2,33 @@
 
 # Ejercicio 1: Crea una lista con los nombres de 3 colores e imprímela.
 # Escribe tu código debajo de esta línea:
-
+colores = ["azul","amarillo","morado"]
+print(colores)
+print()
 
 # Ejercicio 2: Crea una tupla con 2 coordenadas (ej. x, y) e imprímela.
 # (Nota: las tuplas no se pueden modificar una vez creadas)
 # Escribe tu código debajo de esta línea:
 
+mi_tupla = (15,30)
+print(mi_tupla)
+print()
 
 # Ejercicio 3: Crea un conjunto (set) con 3 de tus frutas favoritas e imprímelo.
 # Escribe tu código debajo de esta línea:
+
+frutas_fav ={"mango","fresa","rambután"}
+print(f"Mis frutas favoritas son: {frutas_fav}.")
+print()
 
 
 # Ejercicio 4: Crea un diccionario con la información de un libro ("titulo" y "autor") e imprímelo.
 # Escribe tu código debajo de esta línea:
 
+libro = {
+    
+    "titulo" : "Del amor y otros demonios",
+    "autor" : "Gabriel Garcia Marquez"
+}
+
+print(f"El libro {libro['titulo']} fue escrito por {libro['autor']}.")
